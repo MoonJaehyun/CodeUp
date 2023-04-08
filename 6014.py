28017bb7374c2c11b7332e0c3c049000b376150b
MoonJaehyun <jjmjh214@gmail.com>
2023-04-08 23:18:37 +0900

    add new file

---
+++ 6014.py
@@ -0,0 +1,6 @@
+#실수 1개 입력받아 3번 출력하기
+a = input()
+a = float(a)
+print(a)
+print(a)
+print(a)
---
+++ 6015.py
@@ -0,0 +1,6 @@
+#[기초-입출력] 정수 2개 입력받아 그대로 출력하기2(설명)(py)
+a, b = input().split()
+a=int(a)
+b=int(b)
+print(a)
+print(b)
