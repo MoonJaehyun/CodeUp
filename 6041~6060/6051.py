93af2a1fc3f201f449d1ba1a90c6b928bd750c4e
MoonJaehyun <jjmjh214@gmail.com>
2023-04-12 20:36:48 +0900

    add file 0412

---
+++ 6041~6060/6051.py
@@ -0,0 +1,4 @@
+#6051 : [기초-비교연산] 정수 2개 입력받아 비교하기4(설명)(py)
+a, b = input().split()
+a, b = int(a), int(b)
+print(a!=b)
---
+++ 6041~6060/6057.py
@@ -0,0 +1,7 @@
+#6057 : [기초-논리연산] 참/거짓이 서로 같을 때에만 참 출력하기(설명)(py) 해결
+
+
+a, b = input().split()
+c = bool(int(a))
+d = bool(int(b))
+print(c==d)
